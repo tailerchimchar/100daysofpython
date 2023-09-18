29e224efb04f3a375848a4cf79397982d24687fa
Tailer Nguyen <35902625+tailerchimchar@users.noreply.github.com>
2023-09-18 02:05:58 -0500

    Changed treasureIsland to more creative

--- day3/TreasureIslandGame.py
+++ day3/TreasureIslandGame.py
@@ -26,19 +26,19 @@ print("Your mission is to find the treasure.")
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 #Write your code below this line 👇
-direction= input("left or right?\n")
-if(direction == "left"):
-  swimOrWait = input("swim or wait?\n")
-  if( swimOrWait == "wait"):
-    door = input("Which door? red, blue, or yellow?\n")
-    if(door == "red"):
+choice1= input('You\'re at a crossroads, where do you want to go? Type "left" or "right"?\n').lower()
+if(choice1 == "left"):
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
+  if( choice2 == "wait"):
+    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow, and one blue. Which color do you choose? \n").lower()
+    if(choice3 == "red"):
       print("Burned by fire.\nGame Over.")
-    elif(door == "blue"):
+    elif(choice3 == "blue"):
       print("Eaten by beats.\nGame over.")
-    elif(door == "yellow"):
-      print("You win!")
+    elif(choice3 == "yellow"):
+      print("You found the treasure! You win!")
     else:
-      print("Eaten by beats.\nGame over.")
+      print("You chose a door that doesn't exist.\nGame over.")
   else:
     print("Attacked by trout.\nGame Over.")
 else:
