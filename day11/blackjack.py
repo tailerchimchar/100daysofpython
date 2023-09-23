31f7a7db90d95398f22f06108666db1086198e3a
Tailer Nguyen <35902625+tailerchimchar@users.noreply.github.com>
2023-09-23 03:47:38 -0500

    Keeping game going

--- day11/blackjack.py
+++ day11/blackjack.py
@@ -24,51 +24,61 @@ def play():
   '''
   The play function that starts the game
   '''
-  # give players cards
-  for count in range(num_players):
-    blackjackmechanics.add_player(players, count)
-  blackjackmechanics.give_players_cards(players)
-  dealer = blackjackmechanics.deal_dealer()
-  if blackjackmechanics.instant_blackjack(dealer):
-    print('Unlucky')
-  blackjackmechanics.print_player_and_dealer_hands(players, dealer)
+  keep_playing = True
+  same_players = False
+  while keep_playing:
+    # give players cards
+    if same_players:
+      hi = 000
+    else:
+      for count in range(num_players):
+        blackjackmechanics.add_player(players, count)
+    blackjackmechanics.give_players_cards(players)
+    dealer = blackjackmechanics.deal_dealer()
+    if blackjackmechanics.instant_blackjack(dealer):
+      print('Unlucky')
+    blackjackmechanics.print_player_and_dealer_hands(players, dealer)
 
-  players_in = {}
+    players_in = {}
 
-  # hit, stay, double down, split
-  if blackjackmechanics.instant_blackjack(dealer):
-    for player in players:
-      blackjackmechanics.player_lose(players, player)
-  else:
-    for player in players:
-      current_hand = players[player]["Hand"]
-      print(f"{player}'s current hand is {current_hand}")
-      if players[player]["Already_Won"]:
-        hello = "skipping this line"
-      else:
-        decision = input(f"Would {player.title()} like to 'Hit', 'Stay', 'Double Down', or 'Split'?: ").title()
-      player_is_in = True
-      if decision == "Double Down":
-        players[player]["Bet"] *= 2
-        blackjackplayerchoices.double_down(current_hand)
-      elif decision!= "Stay":
-        player_is_in = blackjackmechanics.play_hand(current_hand, decision)
-      if player_is_in:
-        players_in[player] ={
-          "Name": players[player]["Name"],
-          "Money": players[player]["Money"],
-          "Hand": players[player]["Hand"],
-          "Bet": players[player]["Bet"],
-          "Already_Won": False,
-        } 
-        
-        print("Nice! You make it to the next round.")
-      else:
-        print("Oops. You're out.")
+    # hit, stay, double down, split
+    if blackjackmechanics.instant_blackjack(dealer):
+      for player in players:
         blackjackmechanics.player_lose(players, player)
+    else:
+      for player in players:
+        current_hand = players[player]["Hand"]
+        print(f"{player}'s current hand is {current_hand}")
+        if players[player]["Already_Won"]:
+          hello = "skipping this line"
+        else:
+          decision = input(f"Would {player.title()} like to 'Hit', 'Stay', 'Double Down', or 'Split'?: ").title()
+        player_is_in = True
+        if decision == "Double Down":
+          players[player]["Bet"] *= 2
+          blackjackplayerchoices.double_down(current_hand)
+        elif decision!= "Stay":
+          player_is_in = blackjackmechanics.play_hand(current_hand, decision)
+        if player_is_in:
+          players_in[player] ={
+            "Name": players[player]["Name"],
+            "Money": players[player]["Money"],
+            "Hand": players[player]["Hand"],
+            "Bet": players[player]["Bet"],
+            "Already_Won": False,
+          } 
 
-  #input("Press enter when you're ready for the showdown...")
-  blackjackmechanics.playersVsDealer(players_in, dealer)
+          print("Nice! You make it to the next round.")
+        else:
+          print("Oops. You're out.")
+          blackjackmechanics.player_lose(players, player)
+
+    #input("Press enter when you're ready for the showdown...")
+    blackjackmechanics.playersVsDealer(players_in, dealer)
+    keepplaying = input("Want to keep playing? y or n: ")
+    keep_playing = keepplaying == "y"
+    if keep_playing:
+      same_players = True
 
 
 play()
