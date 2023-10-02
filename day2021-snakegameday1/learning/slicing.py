d3303e21d10a7a5fe2ee1b1eff03b51c9d778510
tailernguyen <tailernguyen@microsoft.com>
2023-10-02 15:51:15 -0500

    FInished snake game :)

---
+++ day2021-snakegameday1/learning/slicing.py
@@ -0,0 +1,11 @@
+piano_keys = ['a','b','c','d','e','f','g']
+
+print(piano_keys[2:5]) # c, d, e
+print(piano_keys[2:]) # c, d, e, f
+print(piano_keys[:5]) #a, b, c, d, e)  
+print(piano_keys[2:5:2]) #c, e
+print(piano_keys[::5]) # a,c,e,g
+print(piano_keys[::-1]) #g,f,e,d,c,b,a
+
+piano_tuple = ('do', 're','mi', 'fa', 'so', 'la', 'ti')
+print(piano_tuple[2:5]) # mi, fa, so
