bf26384e9ccce5a0488102c2bade52b03913eb8c
Tailer Nguyen <35902625+tailerchimchar@users.noreply.github.com>
2023-10-05 05:48:29 -0500

    finished day 25 and created game

---
+++ day25-csvsAndStateGame/learning/squirrelcensusdata.py
@@ -0,0 +1,18 @@
+import pandas
+import csv
+
+data = pandas.read_csv("squirrel_data.csv")
+
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+
+data_dict = {
+  "Fur Color": ["Gray", "Cinnamon", "Black"],
+  "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
+
+#print(data)
